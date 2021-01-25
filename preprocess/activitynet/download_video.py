d329acf94fabeeea67d8a91a408710c9441a0370
Sagiri18 <48347578+Sagiri18@users.noreply.github.com>
2021-01-25 12:04:07 +0800

    update

--- preprocess/activitynet/download_video.py
+++ preprocess/activitynet/download_video.py
@@ -22,7 +22,7 @@ os.system(command1)
 
 for i in videos:
     url = video_database[i]['url']
-    command3 = 'youtube-dl -f bestvideo[ext=mp4]'+'ActivityNetVideos/'+i+' '+url
+    command3 = 'youtube-dl -f '+'ActivityNetVideos/'+i+' '+url
     print(command3)
     os.system(command3)
 
