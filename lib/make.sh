fa26cc8a35844ea8326143ad2b1f08a89f520dec
Sagiri18 <48347578+Sagiri18@users.noreply.github.com>
2021-02-22 10:31:12 +0800

    update

--- lib/make.sh
+++ lib/make.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+export CPATH=/usr/local/cuda/include
+export CUDA_PATH=/usr/local/cuda/
+export PATH=/usr/local/cuda/bin/:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
-CUDA_PATH=/usr/local/cuda/
+export CXXFLAGS="-std=c++11"
+export CFLAGS="-std=c99"
 
 #python setup.py build_ext --inplace
 #rm -rf build
@@ -13,19 +18,19 @@ CUDA_ARCH="-gencode arch=compute_30,code=sm_30 \
            -gencode arch=compute_61,code=sm_61 "
 
 # compile NMS
-cd model/nms/src
+%cd model/nms/src
 echo "Compiling nms kernels by nvcc..."
 nvcc -c -o nms_cuda_kernel.cu.o nms_cuda_kernel.cu \
 	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
 
-cd ../
+%cd ../
 python build.py
 
 # compile roi_temporal_pooling
-cd ../../
-cd model/roi_temporal_pooling/src
+%cd ../../
+%cd model/roi_temporal_pooling/src
 echo "Compiling roi temporal pooling kernels by nvcc..."
 nvcc -c -o roi_temporal_pooling_kernel.cu.o roi_temporal_pooling_kernel.cu \
 	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
-cd ../
+%cd ../
 python build.py
